9dd7ed4d046f20dd615ef80547ede886e0fe2bc4
Rober2BR <rober2br@mail.uc.edu>
2025-07-17 18:16:49 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,7 +13,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // Commented out temporarily
+
+app.MapControllers();
 
 var summaries = new[]
 {
--- obj/Debug/net9.0/GitOutAssignment.AssemblyInfo.cs
+++ obj/Debug/net9.0/GitOutAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitOutAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56c6e208048264ba11b8fe1f18b5b2ca6c57580f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629e27ae089a223d4604e7f903616be55e7e803d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitOutAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitOutAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
